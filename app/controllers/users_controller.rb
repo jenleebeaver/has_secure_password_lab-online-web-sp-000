b98806a52ce370cf8e719a922eefd1a1db27a544
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-13 18:14:37 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,3 @@ class UsersController < ApplicationController
   end
 
 end
-#
-# def create
-#   @user = User.create(user_params)
-#   return redirect_to controller: 'users', action: 'new' unless @user.save
-#   session[:user_id] = @user.id
-#   redirect_to controller: 'welcome', action: 'home'
-# end
