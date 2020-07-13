3e0439031201ecf76b7ab3afe74334e3874a622a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-13 18:20:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to controller: 'user', action: 'welcome'
+      redirect_to controller: 'users', action: 'welcome'
   end
 
 end
